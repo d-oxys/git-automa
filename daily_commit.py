de759379970e5c6ad7dfb1647e92bf633c4b8569
D-oxys <sanuzumaki12@gmail.com>
2024-03-03 09:02:53 +0700

    first commit

---
+++ daily_commit.py
@@ -0,0 +1,22 @@
+from git import Repo
+from datetime import datetime
+import os
+
+# Lokasi direktori repo Anda
+repo_dir = "/path/to/your/repo"
+repo = Repo(repo_dir)
+
+# Membuat file dengan timestamp
+file_name = os.path.join(repo_dir, 'commit.txt')
+with open(file_name, 'w') as fp:
+    fp.write(str(datetime.now()))
+
+# Menambahkan semua perubahan
+repo.git.add('--all')
+
+# Melakukan commit
+repo.index.commit("Daily commit")
+
+# Melakukan push
+origin = repo.remote(name='origin')
+origin.push()
