3f4d6583a8ccc1cc10f91cd33bfcbafcd826b6dc
D-oxys <sanuzumaki12@gmail.com>
2024-03-03 09:25:30 +0700

    Daily commit 1

--- daily_commit.py
+++ daily_commit.py
@@ -1,22 +1,29 @@
 from git import Repo
 from datetime import datetime
 import os
+import time
 
 # Lokasi direktori repo Anda
 repo_dir = "C:/Users/crimson_net/Documents/kodingan/python/git-automa"
 repo = Repo(repo_dir)
 
-# Membuat file dengan timestamp
-file_name = os.path.join(repo_dir, 'commit.txt')
-with open(file_name, 'w') as fp:
-    fp.write(str(datetime.now()))
+file_names = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt']
 
-# Menambahkan semua perubahan
-repo.git.add('--all')
+for i in range(4):
+    # Membuat file dengan timestamp
+    file_name = os.path.join(repo_dir, file_names[i])
+    with open(file_name, 'w') as fp:
+        fp.write(str(datetime.now()))
 
-# Melakukan commit
-repo.index.commit("Daily commit")
+    # Menambahkan semua perubahan
+    repo.git.add('--all')
 
-# Melakukan push
-origin = repo.remote(name='origin')
-origin.push()
+    # Melakukan commit
+    repo.index.commit("Daily commit " + str(i+1))
+
+    # Melakukan push
+    origin = repo.remote(name='origin')
+    origin.push()
+
+    # Tunggu selama 5 detik sebelum commit berikutnya
+    time.sleep(5)
